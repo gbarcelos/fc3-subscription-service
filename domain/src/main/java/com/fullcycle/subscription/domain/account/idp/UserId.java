4022e28502524aea0c7dd4f0cf2e507574d739d6
Gustavo Barcelos <gustavo.barcelos@gmail.com>
2025-03-04 11:08:20 -0300

    Implementação e testes do caso de uso de criar user no idp

--- domain/src/main/java/com/fullcycle/subscription/domain/account/idp/UserId.java
+++ domain/src/main/java/com/fullcycle/subscription/domain/account/idp/UserId.java
@@ -1,14 +1,50 @@
 package com.fullcycle.subscription.domain.account.idp;
 
 import com.fullcycle.subscription.domain.Identifier;
+import java.util.Objects;
 
-public record UserId(String value) implements Identifier {
+public final class UserId implements Identifier<String> {
 
-  public UserId {
+  private final String value;
+
+  private UserId() {
+    this.value = "";
+  }
+
+  public UserId(final String value) {
     this.assertArgumentNotEmpty(value, "'userId' should not be empty");
+    this.value = value;
   }
 
   public static UserId empty() {
-    return new UserId("");
+    return new UserId();
+  }
+
+  @Override
+  public String value() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (obj == null || obj.getClass() != this.getClass()) {
+      return false;
+    }
+    var that = (UserId) obj;
+    return Objects.equals(this.value, that.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
+
+  @Override
+  public String toString() {
+    return "UserId[" +
+        "value=" + value + ']';
   }
 }
