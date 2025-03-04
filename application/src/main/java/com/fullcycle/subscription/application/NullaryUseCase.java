8489f7c679ffaf862310cd38f913d93ebe9ef146
Gustavo Barcelos <gustavo.barcelos@gmail.com>
2025-03-04 10:31:18 -0300

    Adicionando presenter a estrutura de caso de uso

--- application/src/main/java/com/fullcycle/subscription/application/NullaryUseCase.java
+++ application/src/main/java/com/fullcycle/subscription/application/NullaryUseCase.java
@@ -1,5 +1,13 @@
 package com.fullcycle.subscription.application;
 
 public abstract class NullaryUseCase<OUT> {
+
     public abstract OUT execute();
+
+    public <T> T execute(Presenter<OUT, T> presenter) {
+        if (presenter == null) {
+            throw new IllegalArgumentException("NullaryUseCase 'presenter' is required");
+        }
+        return presenter.apply(execute());
+    }
 }
--- application/src/main/java/com/fullcycle/subscription/application/UseCase.java
+++ application/src/main/java/com/fullcycle/subscription/application/UseCase.java
@@ -1,5 +1,13 @@
 package com.fullcycle.subscription.application;
 
 public abstract class UseCase<IN, OUT> {
+
   public abstract OUT execute(IN anIn);
+
+  public <T> T execute(IN in, Presenter<OUT, T> presenter) {
+    if (presenter == null) {
+      throw new IllegalArgumentException("UseCase 'presenter' is required");
+    }
+    return presenter.apply(execute(in));
+  }
 }
